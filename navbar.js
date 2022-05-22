3c86735988cb57a6a171546fa15081f3dc2b5fc7
Catalina Arona <arona.catalina@gmail.com>
2022-05-22 17:11:30 -0300

    Pimer comentario

---
+++ navbar.js
@@ -0,0 +1,6 @@
+var menu = document.querySelector('#menu');
+var menu_bar = document.querySelector('#menu-bar');
+menu_bar.addEventListener('click', function ()
+{
+       menu.classList.toggle('menu-toggle');
+    })
