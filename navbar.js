aae0167c9015a44f6dee2d78abe7306a80aeb26d
Catalina Arona <arona.catalina@gmail.com>
2022-05-25 20:00:27 -0300

    Comentario noche

--- navbar.js
+++ navbar.js
@@ -1,9 +1,22 @@
-var navbar= document.getElementById("#navbar");
-    
-var menubar = document.querySelector("#menubar");
+const navToggle = document.querySelector(".navToggle");
+const navbar = document.querySelector(".navbar");
 
-menubar.addEventListener("click", function () {
 
-    navbar.classList.toggle("menu-toggle");
-    
+navToggle.addEventListener("click", ()=>{
+  navbar.classList.toggle("navbar_visible");
 })
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navBarId").style.top = "0";
+    document.getElementById("navBarId").style.backgroundColor = "#17A899";
+  } else {
+    document.getElementById("navBarId").style.top = "-100px";
+    document.getElementById("navBarId").style.transition = "0.3s";
+    document.getElementById("navBarId").style.backgroundColor = "transparent";
+  }
+  prevScrollpos = currentScrollPos;
+} 
